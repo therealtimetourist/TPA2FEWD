cf9ebf76020ee87c66874063c70646fb3b8767ec
therealtimetourist <timetourst@gmail.com>
2019-07-11 18:02:06 -0400

    examples updated, jQuery doc added

---
+++ examples/randomQuotes/js/app.js
@@ -0,0 +1,12 @@
+// create a variable that targets the quote div
+
+// create a variable that targets the figure image
+
+// create a function to get a random quote
+
+    // get random index for the quotes
+
+    // write a string of HTML that includes the quote and the source to the quote div  
+    
+
+    // write the image to the figure div. use the source for the ALT text
---
+++ examples/randomQuotes/js/quotes.js
@@ -0,0 +1,22 @@
+var quotes = [
+    "When I was a boy of 14, my father was so ignorant... but when I got to be 21, I was astonished at how much he had learned in 7 years.",
+    "Everyone is ignorant, only on different subjects.",
+    "They say such nice things about people at their funerals that it makes me sad that I'm going to miss mine by just a few days.",
+    "What's another word for thesaurus?",
+    "You know more than you know you know... you know?"
+];
+var sources = [
+    "Mark Twain",
+    "Will Rogers",
+    "Garrison Keillor",
+    "Steven Wright",
+    "Don Bedwell"
+];
+
+var figures = [
+    "twain_mark.jpg",
+    "rogers_will.jpg",
+    "keillor_garrison.jpg",
+    "wright_steven.jpg",
+    "bedwell_don.jpg"
+];
