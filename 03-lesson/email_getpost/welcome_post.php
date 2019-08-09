bc05e8277af1c3901fbdd5ec4a5a90d59fbfba70
Don Bedwell <timetourst@gmail.com>
2019-08-09 19:16:09 -0400

    MTA 98-375 Lesson 3
    MTA 98-375 Lesson 3 Added

---
+++ 03-lesson/email_getpost/welcome_post.php
@@ -0,0 +1,13 @@
+﻿<html>
+    <body>
+        <p>
+            Welcome <?php echo $_POST["name"]; ?>
+        </p>
+        <p>
+            Your email address is: <?php echo $_POST["email"]; ?>
+        </p>
+        <p>
+            This was done with a POST method.
+        </p>
+    </body>
+</html>
