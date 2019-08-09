6447830217801c2a50d30f979c69179e7eff3ef2
Don Bedwell <timetourst@gmail.com>
2019-08-09 19:18:29 -0400

    Add files via upload

---
+++ MTA98-375/03-lesson/email_getpost/welcome_get.php
@@ -0,0 +1,13 @@
+﻿<html>
+    <body>
+        <p>
+            Welcome <?php echo $_GET["name"]; ?>
+        </p>
+        <p>
+            Your email address is: <?php echo $_GET["email"]; ?>
+        </p>
+        <p>
+            This was done with a GET method.
+        </p>
+    </body>
+</html>
